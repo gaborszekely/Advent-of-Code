ae4c5c49c1c442dab1ff6968a51db5ea45feaf15
Gabe Szekely <gaborszekely@google.com>
2020-12-04 07:35:51 +0000

    Set up repository and added Day 1 code

---
+++ .history/utils/utils_20201204073344.js
@@ -0,0 +1,4 @@
+F1204 07:33:42.973372 2443415 codemaker.go:136] Please run CodeMaker from within a Piper workspace.
+/**
+ * @fileoverview Description of this file.
+ */
---
+++ .history/utils/utils_20201204073354.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+exports.range = (i, j, inclusive = true) =>
+  Array.from({length: j - i + +inclusive}, (_, idx) => i + idx);
+
+exports.inRange = (min, max) => v => v >= min && v <= max;
+
+exports.assert = (actual, expected) => {
+  if (actual !== expected) {
+    console.error(`Actual value did not match extected value!`);
+    console.log('Actual: ', actual);
+    console.log('Expected: ', expected);
+  }
+}
+
+exports.getInput = (dirname) => fs.readFileSync(`${dirname}/input.txt`, 'utf8');
