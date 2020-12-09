5527da5a7cd220aeeccd8247809c9be54f95a6c4
Gabe Szekely <gaborszekely@google.com>
2020-12-09 07:55:17 +0000

    Finished Day 9.

--- utils/fs.js
+++ utils/fs.js
@@ -5,3 +5,5 @@ exports.__basedir = path.join(__dirname, '..');
 
 exports.getInput = (dirname, file = 'input.txt') =>
     fs.readFileSync(`${dirname}/${file}`, 'utf8').replace(/\n$/, '');
+
+exports.getTestInput = dirname => this.getInput(dirname, 'test.txt');
