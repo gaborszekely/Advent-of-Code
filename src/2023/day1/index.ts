b3e068bd3317921c62d4b3c214276c24f78b9199
Gabe Szekely <gabor_szekely@hotmail.com>
2023-12-01 23:37:50 -0700

    AOC 2023 Day 1

--- src/2023/day1/index.ts
+++ src/2023/day1/index.ts
@@ -4,6 +4,77 @@ import { getInput } from '@utils/fs';
 
 const input = getInput(__dirname);
 
-export function partOne() {}
+const numsMap: Record<string, string> = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seen',
+    8: 'eight',
+    9: 'nine',
+};
 
-export function partTwo() {}
+export function partOne() {
+    return input.split('\n').reduce((acc, row) => {
+        let minFirstIndex = Infinity;
+        let minFirstValue = '';
+        let maxLastIndex = -1;
+        let maxLastValue = '';
+
+        for (const num of Object.keys(numsMap)) {
+            const firstIndex = row.indexOf(num);
+            if (firstIndex >= 0 && firstIndex < minFirstIndex) {
+                minFirstIndex = firstIndex;
+                minFirstValue = num;
+            }
+
+            const lastIndex = row.lastIndexOf(num);
+            if (lastIndex >= 0 && lastIndex > maxLastIndex) {
+                maxLastIndex = lastIndex;
+                maxLastValue = num;
+            }
+        }
+
+        return acc + Number(minFirstValue + maxLastValue);
+    }, 0);
+}
+
+export function partTwo() {
+    return input.split('\n').reduce((acc, row) => {
+        let minFirstIndex = Infinity;
+        let minFirstValue = '';
+        let maxLastIndex = -1;
+        let maxLastValue = '';
+
+        for (const num of Object.keys(numsMap)) {
+            const firstIndex = row.indexOf(num);
+            if (firstIndex >= 0 && firstIndex < minFirstIndex) {
+                minFirstIndex = firstIndex;
+                minFirstValue = num;
+            }
+
+            const firstNumberIndex = row.indexOf(numsMap[num]);
+            if (firstNumberIndex >= 0 && firstNumberIndex < minFirstIndex) {
+                minFirstIndex = firstNumberIndex;
+                minFirstValue = num;
+            }
+
+            const lastIndex = row.lastIndexOf(num);
+            if (lastIndex >= 0 && lastIndex > maxLastIndex) {
+                maxLastIndex = lastIndex;
+                maxLastValue = num;
+            }
+
+            const lastNumberIndex = row.lastIndexOf(numsMap[num]);
+            if (lastIndex >= 0 && lastIndex > maxLastIndex) {
+                maxLastIndex = lastNumberIndex;
+                maxLastValue = num;
+            }
+        }
+
+        return acc + Number(minFirstValue + maxLastValue);
+    }, 0);
+}
